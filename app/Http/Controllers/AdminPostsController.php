06951537a3b332a08a2d709ebc9ba0ed3e0847d1
陳泓維 <tim5556618@gmail.com>
2017-11-27 00:18:31 +0800

    建立lara_0901

---
+++ app/Http/Controllers/AdminPostsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AdminPostsController extends Controller
+{
+    public function index()
+    {
+        $posts=Post::orderBy('created_at','DESC')->get();
+        $data=['posts'=>$posts];
+
+        return view('admin.posts.index');
+    }
+
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    public function edit($id)
+    {
+        $data = ['id' => $id];
+
+        return view('admin.posts.edit', $data);
+    }
+}
