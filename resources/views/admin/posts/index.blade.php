fc75d06e51a1633afb2736e55b459c8635c496f6
陳泓維 <tim5556618@gmail.com>
2017-11-27 22:59:59 +0800

    修改刪除按鈕

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -46,7 +46,12 @@
                         <td>
                             <a href="{{route('admin.posts.edit',$post->id)}}">編輯</a>
                             /
-                            <a href="#">刪除</a>
+                            <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button class="btn btn-link">刪除</button>
+                            </form>
+
                         </td>
                     </tr>
                 @endforeach
