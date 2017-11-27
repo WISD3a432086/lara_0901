5527a89983448000db877e70becc35a5c4136f59
陳泓維 <tim5556618@gmail.com>
2017-11-27 23:11:15 +0800

    編輯 PostsController裡設定 type­hinting

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Http\Requests\PostRequest;
+
 use App\Post;
 
 class AdminPostsController extends Controller
@@ -29,13 +31,13 @@ class AdminPostsController extends Controller
         return view('admin.posts.edit', $data);
     }
 
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
     }
 
-    public function update(Request $request, $id)
+    public function update(PostRequest $request, $id)
      {
          $post = Post::find($id);
          $post-> update($request-> all());
