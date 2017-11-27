63e89640af9ccc5854394fab4c4693361ab86213
陳泓維 <tim5556618@gmail.com>
2017-11-27 23:04:16 +0800

    錯誤修正

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -42,7 +42,7 @@ class AdminPostsController extends Controller
          return redirect()-> route('admin.posts.index');
      }
 
-     public function destory($id)
+     public function destroy($id)
      {
          Post::destroy($id);
          return redirect()->route('admin.posts.index');
